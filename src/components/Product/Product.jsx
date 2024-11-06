a946d92cf2d5981d44fd38b647f3f7ed2a144871
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-06 22:13:30 +0600

    Updated in fake data and set a bg

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,14 +5,14 @@ import ProductDetail from '../ProductDetail/ProductDetail';
 const Product = ({product}) => {
     const {product_id,product_image,product_title, price} = product;
     return (
-        <div>
+        <div className='bg-white p-4'>
              <img src={product_image} className='h-[300px]' alt={product_title} />
-             <h2  className='font-bold'> {product_title} </h2>
-             <h3>Price: {price} </h3>
+             <h2  className='font-bold text-lg'> {product_title} </h2>
+             <h3>Price: {price}k </h3>
              
  
               <Link to={`/product/${product_id}`}>
-                <button className='btn'>View Details</button>
+                <button className='border mt-2 border-primary rounded-[32px] bg-body px-4 py-2 font-bold text-primary  '>View Details</button>
               </Link>
              
              
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ export default {
           primary:'rgb(149, 56, 226)',
           b1: 'rgb(246, 246, 246)',
           details:'rgb(217, 217, 217)',
+          products: 'rgba(9, 8, 15, 0.05)',
+          body:'rgb(247,247,247)',
 
 
 
