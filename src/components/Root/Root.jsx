7a5a388bb682c48b686b99f900f25e013df2ab2e
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-06 20:13:37 +0600

    Resolved a bug and adding conditional rendering in Root.jsx

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,21 +1,39 @@
 import React, { createContext, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export const cartContext = createContext([]);
 export const WishContext = createContext([]);
 
+
+
 const Root = () => {
     const [cart, setCart]= useState([]);
     const [wish, setWish]= useState([]);
+    const location = useLocation();
+ const path= location.pathname;
+ console.log('Root Path',path);
     return (
         <cartContext.Provider value={{ cart, setCart }}>
         <WishContext.Provider value={{ wish, setWish }}>
             <div>
                 <Navbar />
                 <Outlet />
-                <Footer />
+                {/* <Footer /> */}
+                 {
+                    (
+                        (path==='/products') ||
+                        (path==='/statistics') ||
+                        (path==='/dashboard')
+                        
+                    ) && 
+                    <> <Footer></Footer>
+                    </>
+                    
+                } 
+
+
             </div>
         </WishContext.Provider>
     </cartContext.Provider>
